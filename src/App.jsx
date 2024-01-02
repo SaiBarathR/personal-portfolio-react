e274556ecdacd94ee322d00ade324d79ae5bbd35
Sai Barath <saibarathdeep@outlook.com>
2024-01-02 18:16:30 +0530

    Refactor App component to use context and add mode toggle button

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,23 @@
-import React from 'react'
-import { ShaderGradientCanvas, ShaderGradient } from 'shadergradient'
-import * as reactSpring from '@react-spring/three'
-import * as drei from '@react-three/drei'
-import * as fiber from '@react-three/fiber'
+import { useContext } from 'react';
+import ShaderUiProvider from './components/ShaderUiProvider'
+import { ModeContext } from './context/ModeContext';
 import './App.css'
 
 function App() {
 
+  const { mode, dispatch } = useContext(ModeContext);
+  const toggleMode = () => {
+    dispatch({ type: 'SET_MODE', mode: mode === 'dark' ? 'light' : 'dark' })
+  }
+
   return (
-    <div className="App">
-      <ShaderGradientCanvas
-        importedFiber={{ ...fiber, ...drei, ...reactSpring }}
-        style={{
-          position: 'absolute',
-          top: 0,
-        }}
-      >
-        <ShaderGradient
-          control='query'
-          urlString='https://www.shadergradient.co/customize?animate=on&axesHelper=off&brightness=1&cAzimuthAngle=180&cDistance=2.8&cPolarAngle=80&cameraZoom=9.1&color1=%23606080&color2=%238d7dca&color3=%23212121&destination=onCanvas&embedMode=off&envPreset=city&format=gif&fov=45&frameRate=10&gizmoHelper=hide&grain=on&lightType=3d&pixelDensity=1&positionX=0&positionY=0&positionZ=0&range=enabled&rangeEnd=40&rangeStart=0&reflection=0.1&rotationX=50&rotationY=0&rotationZ=-60&shader=defaults&type=waterPlane&uAmplitude=0&uDensity=1.5&uFrequency=0&uSpeed=0.3&uStrength=1.5&uTime=8&wireframe=false'
-        />
-      </ShaderGradientCanvas>
+    <div className={mode === 'dark' ? 'dark bg-black text-white' : 'bg-white text-black h-screen w-full'}>
+      <div className=' flex gap-4'>
+        <button onClick={toggleMode} className='  rounded-md z-[1000]'>
+          Mode : {mode}
+        </button>
+      </div>
+      <ShaderUiProvider />
     </div>
   )
 }
