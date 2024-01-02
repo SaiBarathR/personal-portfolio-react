2d49d62924c7d02b426f557a70139ad05cfb4a72
Sai Barath <saibarathdeep@outlook.com>
2024-01-02 18:37:50 +0530

    Refactor App component and add Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,16 @@
 import { useContext } from 'react';
-import ShaderUiProvider from './components/ShaderUiProvider'
 import { ModeContext } from './context/ModeContext';
+import ShaderUiProvider from './components/ShaderUiProvider'
 import './App.css'
+import Sidebar from './components/Sidebar';
 
 function App() {
 
-  const { theme, grain, dispatch } = useContext(ModeContext);
-  const toggleMode = () => dispatch({ type: 'TOGGLE_MODE' });
-  const toggleGrain = () => dispatch({ type: 'TOGGLE_GRAIN' });
+  const { theme } = useContext(ModeContext);
 
   return (
     <div className={theme === 'dark' ? 'dark bg-black text-white' : 'bg-white text-black h-screen w-full'}>
-      <div className=' flex gap-4'>
-        <button onClick={toggleMode} className='  rounded-md z-[1000]'>
-          theme : {theme}
-        </button>
-        <button onClick={toggleGrain} className='  rounded-md z-[1000]'>
-          grain : {grain ? 'on' : 'off'}
-        </button>
-
-      </div>
+      <Sidebar />
       <ShaderUiProvider />
     </div>
   )
