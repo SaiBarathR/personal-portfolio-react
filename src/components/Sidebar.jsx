6a178fc07369e0056650973ca7c0798c31e5f842
Sai Barath <saibarathdeep@outlook.com>
2024-01-02 19:13:50 +0530

    Refactor theme toggle buttons in Sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,18 +3,25 @@ import { ModeContext } from "../context/ModeContext";
 
 export default function Sidebar() {
 
-    const { theme, grain, dispatch } = useContext(ModeContext);
-    const toggleMode = () => dispatch({ type: 'TOGGLE_MODE' });
+    const { grain, dispatch } = useContext(ModeContext);
     const toggleGrain = () => dispatch({ type: 'TOGGLE_GRAIN' });
+    const toggleMode = (theme) => () => dispatch({ type: 'SET_THEME', theme });
 
     return (
-        <div className=' flex gap-4'>
-            <button onClick={toggleMode} className='  rounded-md z-[1000]'>
-                theme : {theme}
+        <div className=' flex gap-4 items-center h-10'>
+            <button onClick={toggleMode('light')} className='flex gap-2 items-center'>
+                <span className={`w-3 h-3  border border-white bg-black `} />
+                Light
             </button>
-            <button onClick={toggleGrain} className='  rounded-md z-[1000]'>
+            <button onClick={toggleMode('dark')} className='flex gap-2 items-center'>
+                <span className={`w-3 h-3  border  border-black bg-white dark:bg-white`} />
+                Dark
+            </button>
+            <button onClick={toggleGrain} className='flex gap-2 items-center'>
+                <span className={`w-3 h-3  border border-black  dark:border-white  ${grain ? 'bg-black dark:bg-white' : ''}`}>
+                </span>
                 grain : {grain ? 'on' : 'off'}
             </button>
-        </div>
+        </div >
     )
 }
