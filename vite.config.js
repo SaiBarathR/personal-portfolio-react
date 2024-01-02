45b059154bd5f5adc9c1ff73487a49b2cc93c3b9
Sai Barath <saibarathdeep@outlook.com>
2024-01-02 18:16:14 +0530

    Update Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    strictPort: 3000,
+    open: true,
+  },
+  build: {
+    outDir: 'build',
+  },
+  define: {
+    global: 'window'
+  },
+  base: './',
 })
