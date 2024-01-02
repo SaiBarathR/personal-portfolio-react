f1b803f353885b68cc04d1c604f4fff6131e2930
Sai Barath <saibarathdeep@outlook.com>
2024-01-02 18:17:16 +0530

    Add colorSets constant to utils

---
+++ src/utils/constants.js
@@ -0,0 +1,26 @@
+export const colorSets = {
+    dark: {
+        grain: {
+            color1: '#0d0d0d',
+            color2: '#ffffff',
+            color3: '#0d0d0d'
+        },
+        noGrain: {
+            color1: '#2b2b2b',
+            color2: '#2b2b2b',
+            color3: '#000000'
+        }
+    },
+    light: {
+        grain: {
+            color1: '#c4c0c0',
+            color2: '#ffffff',
+            color3: '#adaaaa'
+        },
+        noGrain: {
+            color1: '#c4c0c0',
+            color2: '#ffffff',
+            color3: '#adaaaa'
+        }
+    }
+};
