6bc0f722c5cd74b811a446de3f225bf60acda3f9
Sai Barath <saibarathdeep@outlook.com>
2024-01-03 16:42:29 +0530

    Add toggle monospace functionality to modeReducer

--- src/reducers/modeReducer.jsx
+++ src/reducers/modeReducer.jsx
@@ -4,6 +4,8 @@ export function modeReducer(state, action) {
             return { ...state, theme: action.theme };
         case 'TOGGLE_GRAIN':
             return { ...state, grain: !state.grain };
+        case 'TOGGLE_MONOSPACE':
+            return { ...state, monospace: !state.monospace };
         default:
             throw new Error(`Unhandled action type: ${action.type}`);
     }
