e4a88487433de8cf1239487860db3fb1245e10ba
Sai Barath <saibarathdeep@outlook.com>
2024-01-04 11:11:25 +0530

    Update color constants in constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -6,21 +6,21 @@ export const colorSets = {
             color3: '#0d0d0d'
         },
         noGrain: {
-            color1: '#2b2b2b',
+            color1: '#000000',
             color2: '#2b2b2b',
-            color3: '#000000'
+            color3: '#2b2b2b'
         }
     },
     light: {
         grain: {
             color1: '#c4c0c0',
             color2: '#ffffff',
-            color3: '#adaaaa'
+            color3: '#e6e6e6'
         },
         noGrain: {
-            color1: '#c4c0c0',
-            color2: '#ffffff',
-            color3: '#adaaaa'
+            color1: '#a1a1a1',
+            color2: '#e6e6e6',
+            color3: '#e6e6e6'
         }
     }
 };
