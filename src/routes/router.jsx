98d0f0ad87ad807a9e45fd9294311ad53e913524
Sai Barath <saibarathdeep@outlook.com>
2024-01-04 18:28:00 +0530

    Update router.jsx to use PortfolioContent instead of PortfolioNavHome

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,15 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
-import PortfolioNavHome from "../components/PortfolioNavHome";
+import PortfolioContent from "../components/PortfolioContent";
 import Home from "../components/pages/Home";
 import Projects from "../components/pages/Projects";
 import Info from "../components/pages/Info";
 import Contact from "../components/pages/Contact";
 import UnknownRoute from "../components/UnknownRoute";
 
-export const router = createBrowserRouter([
+export const portfolioContentRouter = createBrowserRouter([
     {
         path: "/",
-        element: <PortfolioNavHome />,
+        element: <PortfolioContent />,
         children: [
             {
                 path: "/home",
