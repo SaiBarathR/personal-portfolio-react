b2b14e286655a97dbf48a8c066044e54b1324cdc
Sai Barath <saibarathdeep@outlook.com>
2024-01-04 15:15:06 +0530

    Add animation styles to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,28 @@ export default {
       },
       lineHeight: {
         'default': '14px',
-      }
+      },
+      animation: {
+        'appear-smooth': 'appear .7s ease-in-out forwards',
+        'disappear-smooth': 'disappear .7s ease-in-out forwards',
+      },
+      keyframes: {
+        appear: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+        disappear: {
+          '0%': {
+            opacity: 1,
+          },
+          '30%': {
+            opacity: 0.2,
+          },
+          '100%': {
+            opacity: 0,
+          },
+        },
+      },
     }
   },
   darkMode: "class"
