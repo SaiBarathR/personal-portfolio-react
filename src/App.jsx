342c75f5236e0528260b7d776f21955898205678
Sai Barath <saibarathdeep@outlook.com>
2024-01-04 18:27:19 +0530

    Add RouterProvider for portfolio content

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { useContext } from 'react';
+import { RouterProvider } from 'react-router-dom';
+import { portfolioContentRouter } from './routes/router';
 import { ModeContext } from './context/ModeContext';
 import ShaderUiProvider from './components/ShaderUiProvider'
-import './App.css'
 import Sidebar from './components/Sidebar';
-import PortfolioContent from './components/PortfolioContent';
+import './App.css'
 
 function App() {
 
@@ -13,7 +14,7 @@ function App() {
     <div className={theme === 'dark' ? 'dark' : ''}>
       <div className={`bg-[#e6e6e6] text-black dark:bg-black dark:text-white h-screen w-full text-default ${monospace ? 'font-NeueMontrealMono' : 'font-NeueMontreal'}`}>
         <ShaderUiProvider />
-        <PortfolioContent />
+        <RouterProvider router={portfolioContentRouter} />
         <Sidebar />
       </div>
     </div >
