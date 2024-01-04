d85c3bf0eac7e2268f3a30ee0b86b64610eda99e
Sai Barath <saibarathdeep@outlook.com>
2024-01-04 18:27:54 +0530

    Add description to Home page

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,5 +1,19 @@
 export default function Home() {
+
+  const description = [
+    "Born in 2000", "in Chennai, India.",
+    "Web development", "has the potential", " to be more ", "varied and creative.",
+    "With a goal", "to showcase", "the opportunities", "of web development,",
+    "On a mission", "to unveil the", "myriad possibilities, ", "I am exploring", "through trials", "and ideas.",
+  ]
+
   return (
-    <div>Home</div>
+    <section className="block opacity-1 w-full min-h-[calc(100vh-4rem)]">
+      <div className="absolute right-14 bottom-14 whitespace-nowrap">
+        <p className="leading-[1.5] flex flex-col font-semibold">
+          {description.map((item, index) => <span key={index}>{item}</span>)}
+        </p>
+      </div>
+    </section>
   )
 }
