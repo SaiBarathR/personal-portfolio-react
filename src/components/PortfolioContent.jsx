1bb621f82a0d2725f4a407fd3372e90135332a8a
Sai Barath <saibarathdeep@outlook.com>
2024-01-04 11:10:18 +0530

    Add PortfolioContent component

---
+++ src/components/PortfolioContent.jsx
@@ -0,0 +1,12 @@
+export default function PortfolioContent() {
+    return (
+        <header className="fixed text-white z-10 left-14 top-12 mix-blend-difference">
+            <h1 className="font-extralight text-3xl h-[30px] ">
+                Sai Barath
+            </h1>
+            <p className="siteHeader_label">
+                RND Software Engineer
+            </p>
+        </header>
+    )
+}
