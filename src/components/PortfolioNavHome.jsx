209cec22b7371886c4329d31e4d057fe11a7012e
Sai Barath <saibarathdeep@outlook.com>
2024-01-04 16:33:28 +0530

    Update PortfolioNavHome component

--- src/components/PortfolioNavHome.jsx
+++ src/components/PortfolioNavHome.jsx
@@ -1,23 +1,31 @@
-import { Link, Outlet, useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import { Link, Outlet, useLocation, useNavigate } from "react-router-dom";
 
 export default function PortfolioNavHome() {
+
     const { pathname } = useLocation();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (pathname === "/") {
+            navigate("/home");
+        }
+    }, [navigate, pathname]);
 
     const navItems = [
-        { path: "/", name: "Home" },
+        { path: "/home", name: "Home" },
         { path: "/projects", name: "Projects" },
         { path: "/info", name: "Info" },
         { path: "/contact", name: "Contact" },
     ];
 
     return (
-        <nav className="siteHeader_nav">
-            <ol>
+        <nav className="mt-8">
+            <ol className="flex flex-col gap-3">
                 {navItems.map((item) => (
                     <li key={item.path}>
-                        <div
-                            className={`${pathname === item.path ? "animate-appear-smooth" : " hidden"} absolute`}
-                        >●
+                        <div className={`${pathname === item.path ? "animate-appear-smooth" : " hidden"} absolute`}>
+                            ●
                         </div>
                         <Link
                             className={`_text ${pathname === item.path ? "animate-disappear-smooth" : "animate-appear-smooth"}`}
