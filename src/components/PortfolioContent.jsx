e256eeb4f7fd3b29883ca8088e70595d886e202a
Sai Barath <saibarathdeep@outlook.com>
2024-01-04 15:15:22 +0530

    Add RouterProvider to PortfolioContent component

--- src/components/PortfolioContent.jsx
+++ src/components/PortfolioContent.jsx
@@ -1,12 +1,16 @@
+import { RouterProvider } from 'react-router-dom';
+import { router } from '../routes/router.jsx';
+
 export default function PortfolioContent() {
     return (
         <header className="fixed text-white z-10 left-14 top-12 mix-blend-difference">
             <h1 className="font-extralight text-3xl h-[30px] ">
                 Sai Barath
             </h1>
-            <p className="siteHeader_label">
+            <p className="font-extralight mt-1 ml-[1px]">
                 RND Software Engineer
             </p>
+            <RouterProvider router={router} />
         </header>
     )
 }
