d4c94a99853d3ea7174278e5fdf9ae6d0229b566
Sai Barath <saibarathdeep@outlook.com>
2024-01-05 12:36:05 +0530

    Refactor PortfolioContent component styling

--- src/components/PortfolioContent.jsx
+++ src/components/PortfolioContent.jsx
@@ -7,10 +7,7 @@ export default function PortfolioContent() {
         <>
             <PortfolioHeader />
             <main className="fixed block z-[2] left-0 top-0 w-full h-full overflow-hidden mix-blend-difference">
-                <div
-                    className="relative min-h-full transform"
-                    style={{ transform: "translate3d(0px,0px,0px)" }}
-                >
+                <div className={`relative min-h-full transform-[translate3d(0px,0px,0px)] text-[#AD9E9E]  dark:text-white`} >
                     <Outlet />
                 </div>
             </main>
