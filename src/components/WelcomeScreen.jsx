a23a161d955a070e3c1156ac4bac2f19e24654bf
Sai Barath <saibarathdeep@outlook.com>
2024-01-14 17:22:22 +0530

    Update font weight for portfolio text

--- src/components/WelcomeScreen.jsx
+++ src/components/WelcomeScreen.jsx
@@ -3,7 +3,7 @@ function WelcomeScreen() {
         <div className="z-[1000] font-NeueMontreal fixed top-0 left-0 gap-2 w-screen h-screen flex items-center justify-center text-white text-4xl bg-black">
             <p className="fade-out" style={{ animationDelay: '0s' }}> Sai</p>
             <p className="fade-out" style={{ animationDelay: '0.3s' }}>Barath</p>
-            <p className="fade-out" style={{ animationDelay: '.7s' }}>Portfolio</p>
+            <p className="fade-out font-extralight" style={{ animationDelay: '.7s' }}>Portfolio</p>
         </div>
     );
 }
