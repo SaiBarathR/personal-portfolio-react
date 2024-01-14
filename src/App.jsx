daf6f39c61e6130a750a4f5546395555c0ade339
Sai Barath <saibarathdeep@outlook.com>
2024-01-14 17:17:19 +0530

    Add WelcomeScreen component and implement show/hide logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { portfolioContentRouter } from './routes/router';
 import { ModeContext } from './context/ModeContext';
 import ShaderUiProvider from './components/ShaderUiProvider'
 import Sidebar from './components/Sidebar';
+import WelcomeScreen from './components/WelcomeScreen';
 import './App.css'
 
 function App() {
 
   const { theme, monospace } = useContext(ModeContext);
+  const [showWelcome, setShowWelcome] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowWelcome(false);
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <div className={theme === 'dark' ? 'dark' : ''}>
+      {showWelcome && <WelcomeScreen />}
       <div className={`bg-[#e6e6e6] fixed text-black dark:bg-black dark:text-white h-screen w-full text-default ${monospace ? 'font-NeueMontrealMono' : 'font-NeueMontreal'}`}>
         <ShaderUiProvider />
         <RouterProvider router={portfolioContentRouter} />
