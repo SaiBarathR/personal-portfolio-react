9bb7b3d2cccc3831bf6c1c3f04a1f8b6b4b7b430
Sai Barath <saibarathr@gmail.com>
2025-12-02 12:58:40 +0530

    Refactor getProjects function to filter repositories by topics and remove unused constants

--- src/hooks/useProjects.jsx
+++ src/hooks/useProjects.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import { reposToDisplay } from "../utils/constants";
 import GitService from "../service/gitService";
 
 export default function useProjects() {
@@ -9,29 +8,30 @@ export default function useProjects() {
     const getProjects = async () => {
         setLoading(true);
         try {
-            let resp = await GitService.getRepos();            
+            let resp = await GitService.getRepos();
 
             if (!resp || (resp.message === "Not Found") || (resp.length !== undefined && resp.length < 1)) {
                 setLoading(false);
-                return console.log("No repos found");                
+                return console.log("No repos found");
             }
-           
-            const sortByRepoToDisplay = reposToDisplay.reduce((acc, repoId) => {
-                const repo = resp.find((repo) => repo.id === repoId);
-                if (!repo) return acc;
-                return [...acc, {
-                    id: repo.id,
-                    name: repo.name ? repo.name.replace(/-/g, " ") : "",
-                    repoUrl: repo.html_url,
-                    updated_at: repo.updated_at,
-                    created_at: repo.created_at ? new Date(repo.created_at).getFullYear() : "",
-                    liveUrl: repo.homepage,
-                    language: repo.language,
-                    topics: repo.topics,
-                }];
-            }, []);
-            
-            setProjects(sortByRepoToDisplay);
+
+            const filterdRepoList = resp.reduce((acc, repo) => {
+                if (repo?.topics?.includes("sbc")) {
+                    return [...acc, {
+                        id: repo.id,
+                        name: repo.name ? repo.name.replace(/-/g, " ") : "",
+                        repoUrl: repo.html_url,
+                        updated_at: repo.updated_at,
+                        created_at: repo.created_at ? new Date(repo.created_at).getFullYear() : "",
+                        liveUrl: repo.homepage,
+                        language: repo.language,
+                        topics: repo.topics,
+                    }];
+                }
+                return acc;
+            }, [])
+
+            setProjects(filterdRepoList);
             setLoading(false);
         }
         catch (error) {
